d881b6a9629e096f72b919063ed2e8955905c9c3
umutboz35 <gamadon21@gmail.com>
2026-06-03 12:14:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'kahvekart-v2';
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/']))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/')))); });
